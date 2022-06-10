a57c95911c4008aebfac4f5dedcbe51ff303b2da
PJunyeong <j_aion@naver.com>
2022-06-10 12:39:41 +0900

    chore: LINT

---
+++ Moasa/Moasa/MoasaApp.swift
@@ -0,0 +1,20 @@
+//
+//  MoasaApp.swift
+//  Moasa
+//
+//  Created by Junyeong Park on 2022/06/10.
+//
+
+import SwiftUI
+
+@main
+struct MoasaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
