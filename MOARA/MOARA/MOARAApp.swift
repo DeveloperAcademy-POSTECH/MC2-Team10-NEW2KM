0ebf5e8cbe292beb5eff7dbf83da0683571a73c4
PJunyeong <j_aion@naver.com>
2022-06-10 09:55:21 +0900

    chore: MOARA created

---
+++ MOARA/MOARA/MOARAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOARAApp.swift
+//  MOARA
+//
+//  Created by Junyeong Park on 2022/06/10.
+//
+
+import SwiftUI
+
+@main
+struct MOARAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
