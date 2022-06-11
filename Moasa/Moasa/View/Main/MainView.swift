63eb268ccd309608b8929027229b2fc21cb2604b
wonhui-kim <0512tini@gmail.com>
2022-06-11 16:53:28 +0900

    Add: 메인 통합[#18]

---
+++ Moasa/Moasa/View/Main/MainView.swift
@@ -0,0 +1,29 @@
+//
+//  MainView.swift
+//  Moasa
+//
+//  Created by 김원희 on 2022/06/11.
+//
+
+import SwiftUI
+
+ struct MainView: View {
+     var body: some View {
+         ZStack {
+             Color("KellyCustomGray").ignoresSafeArea()
+             VStack {
+                 MainTitleView()
+                     .padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0))
+                 TargetContentView()
+                     .padding(EdgeInsets(top: 0, leading: 0, bottom: 39, trailing: 0))
+                 BudgetContentView()
+             }
+         }
+     }
+ }
+
+ struct MainView_Previews: PreviewProvider {
+     static var previews: some View {
+         MainView()
+     }
+ }
