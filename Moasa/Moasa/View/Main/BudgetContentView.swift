9b9102113f183b2bf6f94fae664f5c54a8ec00ac
wonhui-kim <0512tini@gmail.com>
2022-06-11 16:07:44 +0900

    [#10] 메인 게이지 화면

---
+++ Moasa/Moasa/View/Main/BudgetContentView.swift
@@ -0,0 +1,20 @@
+//
+//  BudgetContentView.swift
+//  Moasa
+//
+//  Created by 김원희 on 2022/06/11.
+//
+
+import SwiftUI
+
+struct BudgetContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BudgetContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        BudgetContentView()
+    }
+}
---
+++ Moasa/Moasa/View/Main/MainTitleView.swift
@@ -0,0 +1,20 @@
+//
+//  MainTitleView.swift
+//  Moasa
+//
+//  Created by 김원희 on 2022/06/11.
+//
+
+import SwiftUI
+
+struct MainTitleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainTitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTitleView()
+    }
+}
