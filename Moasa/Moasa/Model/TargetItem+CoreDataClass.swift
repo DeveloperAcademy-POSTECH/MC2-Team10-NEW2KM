6ed49540cb59d3eddcaa4feb9029d7f30df93707
JUNY0110 <98405970+JUNY0110@users.noreply.github.com>
2022-06-12 11:02:53 +0900

    Feat: CoreData 기본세팅
    누락된 CoreData 기본 세팅

---
+++ Moasa/Moasa/Model/TargetItem+CoreDataClass.swift
@@ -0,0 +1,13 @@
+//
+//  TargetItem+CoreDataClass.swift
+//  Moasa
+//
+//  Created by Junyeong Park on 2022/06/10.
+//
+//
+
+import Foundation
+import CoreData
+
+public class TargetItem: NSManagedObject {
+}
---
+++ Moasa/Moasa/Model/TargetItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  TargetItem+CoreDataProperties.swift
+//  Moasa
+//
+//  Created by Junyeong Park on 2022/06/10.
+//
+//
+
+import Foundation
+import CoreData
+
+extension TargetItem {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<TargetItem> {
+        return NSFetchRequest<TargetItem>(entityName: "TargetItem")
+    }
+
+    @NSManaged public var startDate: Date?
+    @NSManaged public var id: UUID?
+    @NSManaged public var targetName: String?
+    @NSManaged public var targetPrice: Int64
+    @NSManaged public var fixedSaving: Int64
+    @NSManaged public var targetImage: Data?
+    @NSManaged public var challengeCycle: Int64
+}
+extension TargetItem: Identifiable {
+}
