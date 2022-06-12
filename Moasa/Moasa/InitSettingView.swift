c635fc6d94b7e3ba178602e71019ed01d82b845a
JUNY0110 <98405970+JUNY0110@users.noreply.github.com>
2022-06-12 11:26:30 +0900

    Add: ImageSettingView 기본 내용 추가
    1. Guiding Text추가
    2. TextField 및 Placeholder추가
    3. Tabview 추가 및 PageTabViewStyle적용

---
+++ Moasa/Moasa/InitSettingView.swift
@@ -0,0 +1,93 @@
+//
+//  InitSettingView.swift
+//  Moasa
+//
+//  Created by 지준용 on 2022/06/12.
+//
+
+import SwiftUI
+
+struct InitSettingView: View {
+    
+    @State var targetName: String = ""
+    @State var targetPrice: String = ""
+    @State var fixedSaving: String = ""
+    
+    var body: some View {
+        TabView {
+            VStack {
+                HStack {
+                    Text("사고 싶은 물건 사진을 넣어주세요.")
+                        .font(.system(size: 25, weight: .semibold))
+                        .padding(.top, 90)
+                    Spacer()
+                }.padding(.leading, 16)
+                Image(systemName: "photo.fill")
+                    .resizable()
+                    .clipShape(Circle())
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 190, height: 190)
+                    .padding(.top, 50)
+
+
+            VStack {
+                HStack {
+                    Text("사고 싶은 물건을 입력해주세요.")
+                        .font(.system(size: 25, weight: .semibold))
+                        .padding(.top, 90)
+                    Spacer()
+                }.padding(.leading, 16)
+
+                TextField("예시: Airpods Max", text: self.$targetName)
+                    .padding(.leading, 16)
+                    .font(.system(size: 17, weight: .regular))
+                    .padding(.top, 140)
+                Spacer()
+            }
+
+            VStack {
+                HStack {
+                    Text("물건 가격을 입력해주세요.")
+                        .font(.system(size: 25, weight: .semibold))
+                        .padding(.top, 90)
+                    Spacer()
+                }.padding(.leading, 16)
+                HStack {
+                    TextField("예시: 1,000,000", text: self.$targetPrice)
+                        .padding(.leading, 16)
+                        .font(.system(size: 17, weight: .regular))
+                    Text("원")
+                        .font(.system(size: 17, weight: .bold))
+                        .padding(.trailing, 16)
+                }.padding(.top, 140)
+                Spacer()
+            }
+
+            VStack {
+                HStack {
+                    Text("월 고정 저금액을 입력해주세요.")
+                        .font(.system(size: 25, weight: .semibold))
+                        .padding(.top, 90)
+                    Spacer()
+                }.padding(.leading, 16)
+                HStack {
+                    TextField("예시: 100,000", text: self.$fixedSaving)
+                        .padding(.leading, 16)
+                        .font(.system(size: 17, weight: .regular))
+                    Text("원")
+                        .font(.system(size: 17, weight: .bold))
+                        .padding(.trailing, 16)
+                }.padding(.top, 140)
+                Spacer()
+                
+            }
+        }.tabViewStyle(PageTabViewStyle())
+    }
+}
+}
+
+struct InitSettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        InitSettingView()
+    }
+}
