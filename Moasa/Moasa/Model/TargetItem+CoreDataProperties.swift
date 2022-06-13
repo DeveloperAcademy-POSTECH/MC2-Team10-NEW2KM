72b931a03de870a661d80bcd1dc7d290ed269b4f
PJunyeong <j_aion@naver.com>
2022-06-13 21:07:33 +0900

    chore: targetItem 관련 TODO

--- Moasa/Moasa/Model/TargetItem+CoreDataProperties.swift
+++ Moasa/Moasa/Model/TargetItem+CoreDataProperties.swift
@@ -32,6 +32,7 @@ extension TargetItem {
         return Int(percentage)
         // 현 시점 게이지를 몇 퍼 센트 모았는지 알려주는 연산 프로퍼티
     }
+    // TODO: targetItem을 위해 모아놓은 저금 히스토리를 기록해야 할 수 있다 (고정저금액 카운트, 유동저금액 카운트, 추가 절약 금액 날짜별 카운트 등)
 }
 
 extension TargetItem: Identifiable {
