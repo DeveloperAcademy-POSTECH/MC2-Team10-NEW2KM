332d135ef1fc964e467c1293ef1a5a44250c95ad
wonhui-kim <0512tini@gmail.com>
2022-06-13 10:03:15 +0900

    Fix: Color+Extension 컬러 추가

--- Moasa/Moasa/Extension/Color+Extension.swift
+++ Moasa/Moasa/Extension/Color+Extension.swift
@@ -9,5 +9,7 @@ import SwiftUI
 
 extension Color {
     static let accentColor = Color("AccentColor")
+    static let kellyCustomGray = Color("KellyCustomGray")
     static let kenCustomOrange = Color("KenCustomOrange")
+    static let systemGray = Color("SystemGray")
 }
