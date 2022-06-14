837561b5caddb5c090623fda6e6d2ba3a42c6e83
JUNY0110 <98405970+JUNY0110@users.noreply.github.com>
2022-06-14 20:38:58 +0900

    feat: 입력 조건 추가
    확인Button클릭해야 다음 TextField입력이 가능하도록 조건문 반영.
    
    배열상의 문제로, 확인을 2번 이상 누르면 Error발생

--- Moasa/Moasa/View/InitSetting/NewInitSettingView.swift
+++ Moasa/Moasa/View/InitSetting/NewInitSettingView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct NewInitSettingView: View {
     @State private var showText = false
+    @State private var showTargetImg = false
 
+    
     @State var targetName: String = ""
+    @State var targetPrice: String = ""
 
     var titleArray: [String] = ["사고 싶은 물건을 입력해주세요", "물건 가격을 입력해주세요", "사고싶은 물건 사진을 넣어주세요"]
     @State var arrayCount: Int = 0
@@ -18,13 +21,26 @@ struct NewInitSettingView: View {
     var body: some View {
         VStack {
             HStack {
-                
                 Text(titleArray[arrayCount])
                     .font(.system(size: 25, weight: .semibold))
                     .padding(.top, 90)
                 Spacer()
             }.padding(.leading, 16)
-            
+            if showText {
+                HStack {
+                    TextField("예시: " + numberFormatter(number: 1_000_000), text: self.$targetPrice)
+                        .padding(.leading, 16)
+                        .font(.system(size: 17, weight: .regular))
+                        .keyboardType(.decimalPad)
+                    Text("원")
+                        .font(.system(size: 17, weight: .bold))
+                        .padding(.trailing, 16)
+                }
+                Divider()
+                    .background(Color.accentColor)
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 20)
+            }
             TextField("예시: Airpods Max", text: self.$targetName)
                 .padding(.leading, 16)
                 .font(.system(size: 17, weight: .regular))
@@ -33,7 +49,24 @@ struct NewInitSettingView: View {
                 .padding(.horizontal, 16)
             Spacer()
 
+            if !targetPrice.isEmpty && !targetName.isEmpty {
                 Button(action: {
+                    showTargetImg = true
+                    arrayCount += 1
+                }, label: {
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 360, height: 60)
+                            .cornerRadius(13)
+                            .foregroundColor(.accentColor)
+                        Text("확인")
+                            .foregroundColor(.white)
+                            .font(.system(size: 20, weight: .bold))
+                    }
+                }).opacity(self.arrayCount < 2 ? 1: 0)
+            } else if !targetName.isEmpty {
+                Button(action: {
+                    showText = true
                     arrayCount += 1
                 }, label: {
                     ZStack {
@@ -46,7 +79,7 @@ struct NewInitSettingView: View {
                             .font(.system(size: 20, weight: .bold))
                     }
                 }).opacity(self.arrayCount < 1 ? 1: 0)
-            
+            }
         }
     }
 }
