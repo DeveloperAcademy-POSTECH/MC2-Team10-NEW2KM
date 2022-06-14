942e6c7ac81cf5e2f99c462976b49f1900fa368f
Taehwan Kim <47441965+obtusa07@users.noreply.github.com>
2022-06-14 12:23:39 +0900

    [#41] Add:heavy_plus_sign: : searchbar add

--- Moasa/Moasa/View/DetailView/SearchBarView.swift
+++ Moasa/Moasa/View/DetailView/SearchBarView.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct SearchBarView: View {
+    let sampleData = ["Garak", "투움바 파스타", "양고기"]
+    var searchResults: [String] {
+        if searchText.isEmpty {
+            return sampleData
+        } else {
+            return sampleData.filter { $0.contains(searchText) }
+        }
+    }
+    @State private var searchText = ""
     var body: some View {
         HStack {
             Text("서치바")
                 .font(.title)
-            Spacer()
+            VStack {
+                NavigationView {
+                    List {
+                        ForEach(searchResults, id: \.self) { name in
+                            NavigationLink(destination: Text(name)) {
+                                Text(name)
+                            }
+                        }
+                    }
+                    .searchable(text: $searchText)
+                }
+                Spacer()
+            }
+            .padding(.leading)
         }
-        .padding(.leading)
     }
 }
 
