3fd00f58c2b9f7ed48a5a44c0758e8611601ccc1
JUNY0110 <98405970+JUNY0110@users.noreply.github.com>
2022-06-14 20:34:32 +0900

    Add: TextField, Button추가
    물건이름 TextField 및
    확인 Button 추가

--- Moasa/Moasa/View/InitSetting/NewInitSettingView.swift
+++ Moasa/Moasa/View/InitSetting/NewInitSettingView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct NewInitSettingView: View {
+    @State private var showText = false
+
+    @State var targetName: String = ""
+
     var titleArray: [String] = ["사고 싶은 물건을 입력해주세요", "물건 가격을 입력해주세요", "사고싶은 물건 사진을 넣어주세요"]
     @State var arrayCount: Int = 0
 
@@ -19,19 +23,30 @@ struct NewInitSettingView: View {
                     .font(.system(size: 25, weight: .semibold))
                     .padding(.top, 90)
                 Spacer()
-                
+            }.padding(.leading, 16)
+            
+            TextField("예시: Airpods Max", text: self.$targetName)
+                .padding(.leading, 16)
+                .font(.system(size: 17, weight: .regular))
+            Divider()
+                .background(Color.accentColor)
+                .padding(.horizontal, 16)
+            Spacer()
+
                 Button(action: {
                     arrayCount += 1
                 }, label: {
-                    Text("확인")
-                })
-            }.padding(.leading, 16)
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 360, height: 60)
+                            .cornerRadius(13)
+                            .foregroundColor(.accentColor)
+                        Text("확인")
+                            .foregroundColor(.white)
+                            .font(.system(size: 20, weight: .bold))
+                    }
+                }).opacity(self.arrayCount < 1 ? 1: 0)
+            
         }
-    }
-}
-
-struct NewInitSettingView_Previews: PreviewProvider {
-    static var previews: some View {
-        NewInitSettingView()
     }
 }
