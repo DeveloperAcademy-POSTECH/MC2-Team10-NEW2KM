f59617ffd171a6c711af60ccd7c5ce0af050b574
JUNY0110 <98405970+JUNY0110@users.noreply.github.com>
2022-06-15 20:53:30 +0900

    Add: 입력완료 버튼, Data 저장기능 추가
    1. MainView로 이동 버튼 추가
    2. Data 저장 기능 추가
    3. 코드 구조 부분 수정
    4. ImagePicker Data Optional반영

--- Moasa/Moasa/View/InitSetting/ConsumedLimitView.swift
+++ Moasa/Moasa/View/InitSetting/ConsumedLimitView.swift
@@ -24,9 +24,9 @@ struct AmountCategories: View {
             HStack {
                 Text(consumedCategory)
                 Spacer()
-                Text(String(consumedAmount))
+                Text(String(numberFormatter(number: consumedAmount)))
             }
-            .font(.system(size: 25, weight: .bold))
+            .font(.system(size: 15, weight: .regular))
             .padding(.vertical, 16)
             .padding(.horizontal, 16)
         }
@@ -43,60 +43,87 @@ struct ConsumedLimitView: View {
                               ConsumedCategories(consumedCategory: "패션/미용", consumedAmount: 300_000)]
     var consumedCategory: [String] = ["식비", "교통비", "쇼핑"]
     var consumedAmount: [Int] = [100_000, 200_000, 300_000]
-
+    @State var fixedSaving: String = ""
     var body: some View {
-        VStack {
-            HStack {
-                Text("예산  항목을 추가해주세요")
-                    .font(.system(size: 25, weight: .semibold))
-                    .padding(.top, 90)
-                    .padding(.bottom, 40)
-                Spacer()
-            }.padding(.leading, 16)
+        NavigationView {
+            VStack {
+                HStack {
+                    Text("예산  항목을 추가해주세요")
+                        .font(.system(size: 25, weight: .semibold))
+                        .padding(.top, 90)
+                        .padding(.bottom, 40)
+                    Spacer()
+                }.padding(.leading, 16)
 
-            Section(header: Text("소비 예산"), content: {
-                ForEach(consumedCategories, id: \.self) { category in
-                    AmountCategories(consumedCategory: category.consumedCategory,
-                                     consumedAmount: category.consumedAmount)
+                Text("목표를 위해 매 월 얼마까지 모을 수 있나요?")
+                    .font(.system(size: 20, weight: .bold))
+                HStack {
+                    TextField("예시: " + numberFormatter(number: 100_000), text: self.$fixedSaving)
+                        .padding(.leading, 16)
+                        .font(.system(size: 17, weight: .regular))
+                        .keyboardType(.decimalPad)
+                    Text("원")
+                        .font(.system(size: 17, weight: .bold))
+                        .padding(.trailing, 16)
                 }
-                .frame(width: 350, height: 100)
+                Divider()
+                    .background(Color.accentColor)
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 20)
+                Section(header: HStack {Text("소비 예산을 설정해주세요")
+                    Spacer()}.font(.system(size: 20, weight: .bold))
+.padding(.horizontal, 16)
+                        , content: {
+                    ForEach(consumedCategories, id: \.self) { category in
+                        AmountCategories(consumedCategory: category.consumedCategory,
+                                         consumedAmount: category.consumedAmount)
+                    }
+                    .frame(width: 350, height: 50)
+                            .cornerRadius(13)
+                    Button(action: {
+// 항목 추가
+                    }, label: {
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.white)
+                                .background(.white)
+                            Image(systemName: "plus")
+                                .font(.system(size: 25, weight: .bold))
+                        }
+                        .frame(width: 350, height: 50)
                         .cornerRadius(13)
-            })
+                        .padding(.horizontal, 16)
+                    })
+                    Spacer()
+                })
+                NavigationLink(destination: ContentView(), label: {
+                    Button(action: {
+// 저장하기
+                    }, label: {
+//                        ZStack {
+//                            Rectangle()
+//                                .frame(width: 360, height: 60)
+//                                .cornerRadius(13)
+//                                .foregroundColor(.accentColor)
+//                            Text("입력 완료")
+//                                .foregroundColor(.white)
+//                                .font(.system(size: 20, weight: .bold))
+//                        }
+                    })
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 360, height: 60)
+                            .cornerRadius(13)
+                            .foregroundColor(.accentColor)
+                        Text("입력 완료")
+                            .foregroundColor(.white)
+                            .font(.system(size: 20, weight: .bold))
+                    }
+                })
+            }
+            .background(Color.kenCustomOrange)
+            .navigationBarHidden(true)
         }
-        .background(Color.kenCustomOrange)
-        .navigationBarHidden(true)
-
-        Button(action: {
-//                let add = ConsumedCategory(context: self.viewContext)
-//                add.consumedCategory = self.consumedCategory
-//                add.consumedLimit = self.consumedLimit
-//                try? self.viewContext.save()
-//                self.dismiss.wrappedValue.dismiss()
-        }, label: {
-            ZStack {
-                Rectangle()
-                    .foregroundColor(.white)
-                    .background(.white)
-                Image(systemName: "plus")
-                    .font(.system(size: 25, weight: .bold))
-//                    HStack {
-//                        VStack {
-//                            TextField("소비항목", text: self.$consumedCategory)
-//                            Spacer()
-//                        }
-//                        Spacer()
-//                        VStack {
-//                            Spacer()
-//                            TextField("소비 예산금액", text: self.$consumedLimit)
-//                        }
-//                    }
-//                    .font(.system(size: 25, weight: .bold))
-//                    .padding(.vertical, 16)
-//                    .padding(.horizontal, 16)
-            }.frame(width: 350, height: 100)
-                .cornerRadius(13)
-        })
-        Spacer()
     }
 }
 
--- Moasa/Moasa/View/InitSetting/NewInitSettingView.swift
+++ Moasa/Moasa/View/InitSetting/NewInitSettingView.swift
@@ -1,142 +1,207 @@
 //
-//  NewInitSettingView.swift
+//  InitSettingView(Toss).swift
 //  Moasa
 //
-//  Created by 지준용 on 2022/06/14.
+//  Created by 지준용 on 2022/06/13.
 //
 
 import SwiftUI
 
 struct NewInitSettingView: View {
+    @Environment(\.managedObjectContext) var viewContext
+    @Environment(\.presentationMode) var dismiss
+
     @State private var showText = false
     @State private var showTargetImg = false
 
     @State var show = false
-    @State var image: Data = .init(count: 1)
     @State var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @State var image: Data? = nil
 
     @State var targetName: String = ""
-    @State var targetPrice: String = ""
-
+    @State var targetPrice: Int64 = 0
+    
     var titleArray: [String] = ["사고 싶은 물건을 입력해주세요", "물건 가격을 입력해주세요", "사고싶은 물건 사진을 넣어주세요"]
     @State var arrayCount: Int = 0
+    
+    @State var lastInput: Bool = false
+    @State var nextView: Bool? = false
 
     var body: some View {
-        VStack {
-            HStack {
-                Text(titleArray[arrayCount])
-                    .font(.system(size: 25, weight: .semibold))
-                    .padding(.top, 90)
-                Spacer()
-            }.padding(.leading, 16)
-            if showTargetImg {
-                if self.image.count != 1 {
-                    Button(action: {
-                        self.show.toggle()
-                    }, label: {
-                        Image(uiImage: UIImage(data: self.image)!)
-                            .resizable()
-                            .clipShape(Circle())
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 190, height: 190)
-                            .padding(.top, 50)
-                    })
-                } else {
-                    Button(action: {
-                        self.show.toggle()
-                    }, label: {
-                        Image(systemName: "photo.fill")
-                            .resizable()
-                            .clipShape(Circle())
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 190, height: 190)
-                            .padding(.top, 50)
-                    })
-                }
-            }
-            if showText {
+        NavigationView {
+            VStack {
                 HStack {
-                    TextField("예시: " + numberFormatter(number: 1_000_000), text: self.$targetPrice)
-                        .padding(.leading, 16)
-                        .font(.system(size: 17, weight: .regular))
-                        .keyboardType(.decimalPad)
-                    Text("원")
-                        .font(.system(size: 17, weight: .bold))
-                        .padding(.trailing, 16)
+                    Text(titleArray[arrayCount])
+                        .font(.system(size: 25, weight: .semibold))
+                        .padding(.top, 90)
+                    Spacer()
+                }.padding(.leading, 16)
+
+                if showTargetImg {
+                    if image == nil {
+                        Button(action: {
+                            self.show.toggle()
+                        }, label: {
+                            Image(systemName: "photo.fill")
+                                .resizable()
+                                .clipShape(Circle())
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 190, height: 190)
+                                .padding(.top, 50)
+                        })
+                    } else {
+                        Button(action: {
+                            self.show.toggle()
+                        }, label: {
+                            Image(uiImage: UIImage(data: self.image!)!)
+                                .resizable()
+                                .clipShape(Circle())
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 190, height: 190)
+                                .padding(.top, 50)
+                        })
+                    }
                 }
+                if showText {
+                    HStack {
+//                        TextField("예시: " + numberFormatter(number: 1_000_000), text: String(self.targetPrice))
+                        TextField("예시: " + numberFormatter(number: 1_000_000),
+                                  value: $targetPrice, formatter: NumberFormatter())
+                            .padding(.leading, 16)
+                            .font(.system(size: 17, weight: .regular))
+                            .keyboardType(.decimalPad)
+                        Text("원")
+                            .font(.system(size: 17, weight: .bold))
+                            .padding(.trailing, 16)
+                    }
+                    Divider()
+                        .background(Color.accentColor)
+                        .padding(.horizontal, 16)
+                        .padding(.bottom, 20)
+                }
+                TextField("예시: Airpods Max", text: self.$targetName)
+                    .padding(.leading, 16)
+                    .font(.system(size: 17, weight: .regular))
                 Divider()
                     .background(Color.accentColor)
                     .padding(.horizontal, 16)
-                    .padding(.bottom, 20)
-            }
-            TextField("예시: Airpods Max", text: self.$targetName)
-                .padding(.leading, 16)
-                .font(.system(size: 17, weight: .regular))
-            Divider()
-                .background(Color.accentColor)
-                .padding(.horizontal, 16)
-            Spacer()
+                Spacer()
 
-            if image.count != 1 {
-                NavigationLink(destination: ConsumedLimitView(), label: {
-                    ZStack{
-                        Button(action: {
+                // 저장한 뒤에 다음 페이지로 넘어가야 한다.
+                if lastInput && self.targetPrice > 0 && !targetName.isEmpty {
+                    NavigationLink(destination: MainView(), tag: true, selection: $nextView) {
+                        EmptyView()
+                    }
+                    Button(action: {
+                        
+                        withAnimation {
+                            let newItem = TargetItem(context: viewContext)
+                            newItem.id = UUID()
+                            newItem.challengeCycle = 0
+                            newItem.fixedSaving = 100
+                            newItem.startDate = Date()
+                            newItem.targetImage = image
+                            newItem.targetPrice = targetPrice
+                            newItem.targetName = targetName
+                            newItem
+                            // 초기 세팅 -> 정보 입력, TargetItem 생성
+                            do {
+                                try viewContext.save()
+                            } catch {
+                                let nsError = error as NSError
+                                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                            }
                             
-                        }, label: {
-                            Rectangle()
-                                .frame(width: 360, height: 60)
-                                .cornerRadius(13)
-                                .foregroundColor(.accentColor)
-                            Text("다음")
-                                .foregroundColor(.white)
-                                .font(.system(size: 20, weight: .bold))
-                        })
+                        }
+                        nextView = true
+//                        let add = TargetItem(context: viewContext)
+//                        add.id = UUID()
+//                        add.targetName = targetName
+//                        add.targetPrice = targetPrice
+//                        add.targetImage = image
+//                        add.challengeCycle = 0
+//                        add.fixedSaving = 0
+//                        add.startDate = Date()
+//                        try? self.viewContext.save()
+//                        nextView = true
+//                        print("저장 완료")
+                    }, label: {
                         ZStack {
                             Rectangle()
                                 .frame(width: 360, height: 60)
                                 .cornerRadius(13)
                                 .foregroundColor(.accentColor)
-                            Text("다음")
+                            Text("이미지 저장하기")
                                 .foregroundColor(.white)
                                 .font(.system(size: 20, weight: .bold))
                         }
-                    }
-                }).disabled(!self.targetName.isEmpty && !self.targetPrice.isEmpty && self.image.count != 1 ? false : true)
-            } else if !targetPrice.isEmpty && !targetName.isEmpty {
-                Button(action: {
-                    showTargetImg = true
-                    arrayCount += 1
-                }, label: {
-                    ZStack {
-                        Rectangle()
-                            .frame(width: 360, height: 60)
-                            .cornerRadius(13)
-                            .foregroundColor(.accentColor)
-                        Text("확인")
-                            .foregroundColor(.white)
-                            .font(.system(size: 20, weight: .bold))
-                    }
-                }).opacity(self.arrayCount < 2 ? 1: 0)
-            } else if !targetName.isEmpty {
-                Button(action: {
-                    showText = true
-                    arrayCount += 1
-                }, label: {
-                    ZStack {
-                        Rectangle()
-                            .frame(width: 360, height: 60)
-                            .cornerRadius(13)
-                            .foregroundColor(.accentColor)
-                        Text("확인")
-                            .foregroundColor(.white)
-                            .font(.system(size: 20, weight: .bold))
-                    }
-                }).opacity(self.arrayCount < 1 ? 1: 0)
+                    })
+//                    NavigationLink(destination: Text("destination"), label: {
+//                        ZStack {
+//                            Button(action: {
+//                                let add = TargetItem(context: viewContext)
+//                                add.id = UUID()
+//                                add.targetName = targetName
+//                                add.targetPrice = targetPrice
+//                                add.targetImage = image
+//                                add.challengeCycle = 0
+//                                add.fixedSaving = 0
+//                                add.startDate = Date()
+//
+//                                try? self.viewContext.save()
+//                                print("저장 완료")
+//
+//
+//                            }, label: {
+//                                ZStack {
+//                                    Rectangle()
+//                                        .frame(width: 360, height: 60)
+//                                        .cornerRadius(13)
+//                                        .foregroundColor(.accentColor)
+//                                    Text("이미지 저장하기")
+//                                        .foregroundColor(.white)
+//                                        .font(.system(size: 20, weight: .bold))
+//                                }
+//                            })
+//                        }
+//                    }
+                } else if !lastInput && self.targetPrice > 0 && !self.targetName.isEmpty {
+                    Button(action: {
+                        showTargetImg = true
+                        lastInput = true
+                        arrayCount += 1
+                    }, label: {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 360, height: 60)
+                                .cornerRadius(13)
+                                .foregroundColor(.accentColor)
+                            Text("확인")
+                                .foregroundColor(.white)
+                                .font(.system(size: 20, weight: .bold))
+                        }
+                    }).opacity(self.arrayCount < 2 ? 1: 0)
+                } else if !self.targetName.isEmpty {
+                    Button(action: {
+                        showText = true
+                        arrayCount += 1
+                    }, label: {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 360, height: 60)
+                                .cornerRadius(13)
+                                .foregroundColor(.accentColor)
+                            Text("확인")
+                                .foregroundColor(.white)
+                                .font(.system(size: 20, weight: .bold))
+                        }
+                    }).opacity(self.arrayCount < 1 ? 1: 0)
+                }
             }
-        }
-        .navigationBarHidden(true)
-        .sheet(isPresented: self.$show, content: {
-            ImagePicker(images: self.$image, show: self.$show, sourceType: self.sourceType)
-        })
+            .sheet(isPresented: self.$show, content: {
+                ImagePicker(images: $image, show: self.$show, sourceType: self.sourceType)
+            })
+        }.background(Color.kenCustomOrange)
+            .navigationBarHidden(true)
     }
 }
