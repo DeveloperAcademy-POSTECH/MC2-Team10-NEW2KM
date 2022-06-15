d1cc6cf9dcf2414ae5ce0e43258eff67363f59bd
JUNY0110 <98405970+JUNY0110@users.noreply.github.com>
2022-06-16 03:27:33 +0900

    Refactor: ConsumedLimitView 코드 리팩토링 및 구조변경
    Refactor: ConsumedLimitView 코드 리팩토링 및 구조변경

--- Moasa/Moasa/View/InitSetting/ConsumedLimitView.swift
+++ Moasa/Moasa/View/InitSetting/ConsumedLimitView.swift
@@ -7,126 +7,164 @@
 
 import SwiftUI
 
-struct ConsumedCategories: Hashable {
-    var consumedCategory: String
-    var consumedAmount: Int
-}
-
-struct AmountCategories: View {
-    var consumedCategory: String
-    var consumedAmount: Int
+struct BtnShape: View {
+    @Binding var btnText: String
 
     var body: some View {
         ZStack {
             Rectangle()
+                .frame(width: 360, height: 60)
+                .cornerRadius(13)
+                .foregroundColor(.accentColor)
+            Text(btnText)
                 .foregroundColor(.white)
-                .background(.white)
-            HStack {
-                Text(consumedCategory)
-                Spacer()
-                Text(String(numberFormatter(number: consumedAmount)))
-            }
-            .font(.system(size: 15, weight: .regular))
-            .padding(.vertical, 16)
-            .padding(.horizontal, 16)
+                .font(.system(size: 20, weight: .bold))
         }
-        .frame(width: 350, height: 100)
-        .cornerRadius(13)
     }
+}
+struct ConsumedCategories: View {
+    @Binding var consumedCategory: String
+    @Binding var consumedLimit: Int
+
+    var body: some View {
+        HStack {
+            VStack {
+                TextField("기타", text: $consumedCategory)
+                Divider()
+                    .background(Color.accentColor)
+            }
+            VStack {
+                HStack {
+                    TextField("100_000", value: $consumedLimit, formatter: NumberFormatter())
+                        .keyboardType(.decimalPad)
+                    Text("원")
+                        .font(.system(size: 17, weight: .regular))
+                        .padding(.trailing, 16)
+                }
+                Divider()
+                    .background(Color.accentColor)
+            }
+        }
+    }
+}
+
+struct TestArray: Hashable {
+    var test1: String
+    var test2: Int
 }
 
 struct ConsumedLimitView: View {
     @Environment(\.managedObjectContext) var viewContext
 
-    var consumedCategories = [ConsumedCategories(consumedCategory: "식비", consumedAmount: 100_000),
-                              ConsumedCategories(consumedCategory: "교통/차량", consumedAmount: 200_000),
-                              ConsumedCategories(consumedCategory: "패션/미용", consumedAmount: 300_000)]
-    var consumedCategory: [String] = ["식비", "교통비", "쇼핑"]
-    var consumedAmount: [Int] = [100_000, 200_000, 300_000]
-    @State var fixedSaving: String = ""
+    @State var consumedCategories = ["식비", "교통/차량", "패션/미용"]
+    @State var consumedLimits = [500_000, 100_000, 200_000]
+
+    @State var consumedCategoryArray = ["기타", "기타", "기타", "기타", "기타", "기타", "기타"]
+    @State var consumedLimitArray = [0, 0, 0, 0, 0, 0, 0]
+
+    @State var consumedCategory = ""
+    @State var consumedLimit: Int = 0
+    @State var addCategory = false
+    @State var addArray: Int = 0
+
+    @State var showList = false
+    @State var nextView: Bool? = false
+
+    @State private var fixedSaving = 0
+    @State var btnText: [String] = ["확인", "입력 완료"]
+
+    var testArray = [TestArray(test1: "", test2: 0),
+                     TestArray(test1: "", test2: 0)]
+
     var body: some View {
         NavigationView {
             VStack {
-                HStack {
-                    Text("예산  항목을 추가해주세요")
-                        .font(.system(size: 25, weight: .semibold))
-                        .padding(.top, 90)
-                        .padding(.bottom, 40)
-                    Spacer()
-                }.padding(.leading, 16)
+                if showList {
+                    HStack {
+                        Text("예산 항목을 추가해주세요")
+                            .font(.system(size: 25, weight: .semibold))
+                            .padding(.top, 90)
+                            .padding(.bottom, 40)
+                        Spacer()
+                    }.padding(.leading, 16)
 
-                Text("목표를 위해 매 월 얼마까지 모을 수 있나요?")
-                    .font(.system(size: 20, weight: .bold))
-                HStack {
-                    TextField("예시: " + numberFormatter(number: 100_000), text: self.$fixedSaving)
-                        .padding(.leading, 16)
-                        .font(.system(size: 17, weight: .regular))
-                        .keyboardType(.decimalPad)
-                    Text("원")
-                        .font(.system(size: 17, weight: .bold))
-                        .padding(.trailing, 16)
-                }
-                Divider()
-                    .background(Color.accentColor)
-                    .padding(.horizontal, 16)
-                    .padding(.bottom, 20)
-                Section(header: HStack {Text("소비 예산을 설정해주세요")
-                    Spacer()}.font(.system(size: 20, weight: .bold))
-.padding(.horizontal, 16)
-                        , content: {
-                    ForEach(consumedCategories, id: \.self) { category in
-                        AmountCategories(consumedCategory: category.consumedCategory,
-                                         consumedAmount: category.consumedAmount)
-                    }
-                    .frame(width: 350, height: 50)
-                            .cornerRadius(13)
-                    Button(action: {
-// 항목 추가
-                    }, label: {
-                        ZStack {
-                            Rectangle()
-                                .foregroundColor(.white)
-                                .background(.white)
-                            Image(systemName: "plus")
-                                .font(.system(size: 25, weight: .bold))
-                        }
-                        .frame(width: 350, height: 50)
-                        .cornerRadius(13)
-                        .padding(.horizontal, 16)
-                    })
-                    Spacer()
-                })
-                NavigationLink(destination: ContentView(), label: {
-                    Button(action: {
-// 저장하기
-                    }, label: {
-//                        ZStack {
-//                            Rectangle()
-//                                .frame(width: 360, height: 60)
-//                                .cornerRadius(13)
-//                                .foregroundColor(.accentColor)
-//                            Text("입력 완료")
-//                                .foregroundColor(.white)
-//                                .font(.system(size: 20, weight: .bold))
-//                        }
-                    })
-                    ZStack {
-                        Rectangle()
-                            .frame(width: 360, height: 60)
-                            .cornerRadius(13)
-                            .foregroundColor(.accentColor)
-                        Text("입력 완료")
-                            .foregroundColor(.white)
+                    Section(header: HStack {
+                        Text("소비 예산을 설정해주세요")
                             .font(.system(size: 20, weight: .bold))
+                        Spacer()
+                        Button(action: {
+                            self.addArray += 1
+                        }, label: {
+                            Image(systemName: "plus")
+                        })
+                    }.padding(.horizontal, 16), content: {
+// 어떻게 해야 버튼을 누르면 7개가 차근차근 생길까
+                        VStack {
+                            if addArray < 7 {
+                                ConsumedCategories(consumedCategory: $consumedCategoryArray[addArray],
+                                                   consumedLimit: $consumedLimitArray[addArray])
+                            }
+                            ConsumedCategories(consumedCategory: $consumedCategories[0],
+                                               consumedLimit: $consumedLimits[0]) // 식비(Default)
+                            ConsumedCategories(consumedCategory: $consumedCategories[1],
+                                               consumedLimit: $consumedLimits[1]) // 교통/차량(Default)
+                            ConsumedCategories(consumedCategory: $consumedCategories[2],
+                                               consumedLimit: $consumedLimits[2]) // 패션/미용(Default)
+                        }
+                        .padding(.horizontal, 16)
+                        .padding(.bottom, 40)
+                    })
+                }
+// 저장 후 메인뷰 넘어가기
+                if consumedCategories.count > 3 && self.fixedSaving > 0 {
+                    NavigationLink(destination: MainView(), tag: true, selection: $nextView) {
+                        EmptyView()
                     }
-                })
+
+                    Button(action: {
+                        // 저장기능
+                    }, label: {
+                        BtnShape(btnText: $btnText[1])
+                    })
+// 고정 저금액 설정
+                } else if self.fixedSaving >= 0 {
+                    HStack {
+                        Text("목표를 위해 매 월 얼마까지 모을 수 있나요?")
+                            .font(.system(size: 20, weight: .bold))
+                        Spacer()
+                    }.padding(.leading, 16)
+
+                    HStack {
+                        TextField("예시: " + numberFormatter(number: 100_000),
+                                  value: $fixedSaving, formatter: NumberFormatter())
+                            .padding(.leading, 16)
+                            .font(.system(size: 17, weight: .regular))
+                            .keyboardType(.decimalPad)
+                        Text("원")
+                            .font(.system(size: 17, weight: .regular))
+                            .padding(.trailing, 16)
+                    }
+                    Divider()
+                        .background(Color.accentColor)
+                        .padding(.horizontal, 16)
+                        .padding(.bottom, 20)
+
+// 예산항목 추가를 보여주는 버튼
+                    Button(action: {
+                        showList = true
+                    }, label: {
+                        BtnShape(btnText: $btnText[0])
+                    })
+                    .disabled(self.fixedSaving > 0 ? false : true)
+                    .opacity(self.showList ? 0: 1)
+                }
             }
             .background(Color.kenCustomOrange)
             .navigationBarHidden(true)
         }
+        .background(Color.kenCustomOrange)
     }
 }
-
 struct ConsumedLimitView_Previews: PreviewProvider {
     static var previews: some View {
         ConsumedLimitView()
