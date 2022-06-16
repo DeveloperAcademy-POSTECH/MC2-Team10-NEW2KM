edc818677a16d2e719cd4e0765ffe7f33948f9a6
PJunyeong <j_aion@naver.com>
2022-06-16 14:02:07 +0900

    feat: Init -> Main

--- Moasa/Moasa/MoasaApp.swift
+++ Moasa/Moasa/MoasaApp.swift
@@ -10,19 +10,19 @@ import SwiftUI
 @main
 struct MoasaApp: App {
     @AppStorage("initSetting") var initSetting: Bool = UserDefaults.standard.bool(forKey: "initSetting")
-    @StateObject var items:Items = Items()
+    // 초기 세팅 필요 시 NewInitSettingView
+    @StateObject var items: Items = Items()
     var body: some Scene {
         WindowGroup {
             NavigationView {
                 if initSetting {
                     MainView()
                         .environmentObject(items)
-                        .onAppear(perform: items.load)
                 } else {
                     NewInitSettingView()
                         .environmentObject(items)
                 }
-            }
+            }.onAppear(perform: items.load)
         }
     }
 }
